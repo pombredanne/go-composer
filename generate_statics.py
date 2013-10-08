269b483af5551bfea2d7d686445a6e602d72bd39
matee <matee@matee.net>
2013-10-08 11:20:51 +0200

    pylint: Your code has been rated at 10.00/10 (previous run: 9.38/10, +0.62)

--- generate_statics.py
+++ generate_statics.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+"""
+Simple script to generate sample statics for VoD and with ordering.
+All data will be stored in ./static directory
+"""
+
 import json
 import random
 
@@ -8,8 +13,9 @@ MAX = 10000
 CATEGORIES = [2000, 3000, 4000, 5000, 6000]
 
 
-def main():
-    map(lambda i: json.dump(
+def store_vod(i):
+    """Prepare dict for VoD object, and store it in file."""
+    json.dump(
         {
             'id': i,
             'title': 'title%s' % i,
@@ -17,7 +23,15 @@ def main():
                 CATEGORIES,
                 random.randint(1, 2))
         },
-        open('./static/vods%s.json' % i, 'w')), range(MAX))
+        open('./static/vods%s.json' % i, 'w')
+    )
+
+
+def main():
+    """Entry point :) Generates VoD and sorting files"""
+    # pylint: disable=W0141
+    map(store_vod, range(MAX))  # Screw you pylint,
+                                # it looks better than list comprehension
     sorting = {}
     for i in range(10):
         sorting[i] = random.sample(range(MAX), random.randint(1000, 10000))
